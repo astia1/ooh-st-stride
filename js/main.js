9b544820abaf196c933ab00d8a3f81c060dc3287
“Astia1” <john@mckinneyphoto.com>
2023-01-18 14:00:03 -0800

    edits-1

--- js/main.js
+++ js/main.js
@@ -23,28 +23,3 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunctionx() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
-window.onscroll = function () {
-    myFunction();
-};
-
-var navbar = document.getElementById("myTopnav");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
